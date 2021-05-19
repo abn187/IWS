eae47820cce96d1c7bdef690840cf4053cb212ac
My <nguyenmy1023@gmail.com>
2021-05-19 21:48:40 +0700

    delete poll

--- news/application/config/autoload.php
+++ news/application/config/autoload.php
@@ -134,5 +134,5 @@ $autoload['language'] = array();
 $autoload['model'] = array('auth_model', 'settings_model', 'visual_settings_model', 'page_model', 'post_model', 'post_admin_model',
     'ad_model', 'category_model', 'tag_model', 'post_file_model', 'comment_model',
     'gallery_category_model', 'gallery_model', 'reading_list_model', 'contact_model', 'newsletter_model',
-    'widget_model', 'poll_model', 'navigation_model', 'rss_model', 'file_model', 'language_model', 'reaction_model', 'profile_model', 'post_item_model',
+    'widget_model', 'navigation_model', 'rss_model', 'file_model', 'language_model', 'reaction_model', 'profile_model', 'post_item_model',
     'quiz_model');
--- news/application/views/admin/includes/_header.php
+++ news/application/views/admin/includes/_header.php
@@ -283,21 +283,6 @@
                         </ul>
                     </li>
                 <?php endif; ?>
-                <?php if (check_user_permission('polls')): ?>
-                    <li class="treeview">
-                        <a href="#">
-                            <i class="fa fa-list"></i> <span><?php echo trans("polls"); ?></span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
-                        </a>
-                        <ul class="treeview-menu">
-                            <li>
-                                <a href="<?php echo admin_url(); ?>add-poll"><?php echo trans("add_poll"); ?></a>
-                            </li>
-                            <li>
-                                <a href="<?php echo admin_url(); ?>polls"><?php echo trans("polls"); ?></a>
-                            </li>
-                        </ul>
-                    </li>
-                <?php endif; ?>
                 <?php if (check_user_permission('gallery')): ?>
                     <li class="treeview">
                         <a href="#">
